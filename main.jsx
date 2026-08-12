8475c60375458d02901829dc3906bcaa8f0a1770
anchon1o <anchonio@gmail.com>
2026-08-12 08:09:05 +0200

    adios envlocal

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ImproApp from './ImproApp.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ImproApp />
-  </StrictMode>,
-)
